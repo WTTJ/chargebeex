5b30f2b38a6427908c85bb1ad2f653967b6c9cbe
Maxime MENAGER <maxime.menager@wttj.co>
2022-03-28 17:53:59 +0200

    fix: remove unused module
    Signed-off-by: Maxime Ménager <maxime.menager@wttj.co>

--- test/support/fixtures/webhook.ex
+++
@@ -1,9 +0,0 @@
-defmodule Chargebeex.Fixtures.Webhook do
-  alias Chargebeex.Fixtures.Event, as: FixtureEvent
-
-  def customer_updated(params \\ %{}) do
-    FixtureEvent.customer_updated()
-    |> Map.get("event")
-    |> Map.merge(params)
-  end
-end
