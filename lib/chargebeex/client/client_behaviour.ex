219cc3a5fe1617e322c2f0da55a2ed2028293a54
Maxime MENAGER <maxime.menager@netc.eu>
2022-04-11 18:18:54 +0200

    chore: add doc for Client behaviour
    Signed-off-by: Maxime Ménager <maxime.menager@wttj.co>

--- lib/chargebeex/client/client_behaviour.ex
+++ lib/chargebeex/client/client_behaviour.ex
@@ -1,4 +1,7 @@
 defmodule Chargebeex.ClientBehaviour do
+  @moduledoc """
+    Define the callbacks for a client to get the data from Chargebee.
+  """
   @callback get(url :: binary(), body :: binary(), headers :: list()) ::
               {:ok, integer(), list(), any()} | {:error, integer(), list(), any()}
   @callback post(url :: binary(), body :: binary(), headers :: list()) ::
