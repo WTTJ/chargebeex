eacb5ce5f41c8914a47b2da38bc9609bb968bd03
Igor Fernandes <igorrfc10@gmail.com>
2025-01-13 09:31:03 -0300

    Add sub entitlement's set_availability action
    to SubscriptionEntitlement module.

--- test/chargebeex/subscription_entitlement_test.exs
+++ test/chargebeex/subscription_entitlement_test.exs
@@ -5,6 +5,7 @@ defmodule Chargebeex.SubscriptionEntitlementTest do
 
   alias Chargebeex.Fixtures.Common
   alias Chargebeex.SubscriptionEntitlement
+  alias Chargebeex.Fixtures.SubscriptionEntitlement, as: SubscriptionEntitlementFixture
 
   setup :verify_on_exit!
 
@@ -77,6 +78,74 @@ defmodule Chargebeex.SubscriptionEntitlementTest do
                  "feature_type[is]" => "switch",
                  limit: 1
                })
+    end
+  end
+
+  describe "set_availability/3" do
+    test "with bad authentication should fail" do
+      unauthorized = Common.unauthorized()
+
+      expect(
+        Chargebeex.HTTPClientMock,
+        :post,
+        fn url, body, headers ->
+          assert url ==
+                   "https://test-namespace.chargebee.com/api/v2/subscriptions/subscription_id/subscription_entitlements/set_availability"
+
+          assert headers == [
+                   {"Authorization", "Basic dGVzdF9jaGFyZ2VlYmVlX2FwaV9rZXk6"},
+                   {"Content-Type", "application/x-www-form-urlencoded"}
+                 ]
+
+          assert body == "is_enabled=true"
+
+          {:ok, 401, [], Jason.encode!(unauthorized)}
+        end
+      )
+
+      params = %{
+        "is_enabled" => true,
+        "subscription_entitlements" => []
+      }
+
+      assert {:error, 401, [], ^unauthorized} =
+               SubscriptionEntitlement.set_availability("subscription_id", params)
+    end
+
+    test "with valid data should succeed" do
+      expect(
+        Chargebeex.HTTPClientMock,
+        :post,
+        fn url, body, headers ->
+          assert url ==
+                   "https://test-namespace.chargebee.com/api/v2/subscriptions/subscription_id/subscription_entitlements/set_availability"
+
+          assert headers == [
+                   {"Authorization", "Basic dGVzdF9jaGFyZ2VlYmVlX2FwaV9rZXk6"},
+                   {"Content-Type", "application/x-www-form-urlencoded"}
+                 ]
+
+          assert body == "is_enabled=true&subscription_entitlements[feature_id][0]=foo_feature_id"
+
+          {:ok, 200, [], SubscriptionEntitlementFixture.list()}
+        end
+      )
+
+      subscription_id = "BTLybZUInBGCXDMY"
+
+      params = %{
+        "is_enabled" => true,
+        "subscription_entitlements" => [
+          %{
+            "feature_id" => "shore-bm-customer-feedback"
+          }
+        ]
+      }
+
+      Chargebeex.SubscriptionEntitlement.set_availability(subscription_id, params)
+
+      assert {:ok, [%SubscriptionEntitlement{}], %{"next_offset" => _}} =
+               SubscriptionEntitlement.set_availability(subscription_id, params)
     end
   end
 end
