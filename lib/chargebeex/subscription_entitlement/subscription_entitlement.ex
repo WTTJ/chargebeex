8b670d38b5596c966de20c1873961fedc677ce7e
Igor Fernandes <igorrfc10@gmail.com>
2025-01-13 14:28:00 -0300

    Add SubscriptionEntitlement's set_availability support (#145)
    * Add sub entitlement's set_availability action
    
    to SubscriptionEntitlement module.
    
    * improve tests
    
    * update doc

--- lib/chargebeex/subscription_entitlement/subscription_entitlement.ex
+++ lib/chargebeex/subscription_entitlement/subscription_entitlement.ex
@@ -13,6 +13,22 @@ defmodule Chargebeex.SubscriptionEntitlement do
   """
   @type feature_type :: String.t()
 
+  @typedoc """
+  A single subscription entitlement map.
+  """
+  @type subscription_entitlement :: %{
+          required(:feature_id) => String.t()
+        }
+
+  @typedoc """
+  Parameters for the `set_availability` function.
+  """
+  @type set_availability_params :: %{
+          required(:is_enabled) => boolean(),
+          optional(:subscription_entitlements) => [subscription_entitlement()],
+          optional(any()) => any()
+        }
+
   typedstruct do
     field :subscription_id, String.t()
     field :feature_id, String.t()
@@ -47,5 +63,57 @@ defmodule Chargebeex.SubscriptionEntitlement do
       params,
       opts
     )
+  end
+
+  @doc """
+  Enables or disables specific subscription_entitlements for a subscription.
+
+
+  For more info, check the API doc: https://apidocs.eu.chargebee.com/docs/api/subscription_entitlements?lang=curl#enable/disable_subscription_entitlements
+
+  ## Examples
+  #
+      iex(1)> subscription_id = "BTLybZUInBGCXDMY"
+      "BTLybZUInBGCXDMY"
+      iex(2)> params = %{
+      ...(2)>   "is_enabled" => true,
+      ...(2)>   "subscription_entitlements" => [
+      ...(2)>     %{
+      ...(2)>       "feature_id" => "foo_feature"
+      ...(2)>     }
+      ...(2)>   ]
+      ...(2)> }
+      %{
+        "is_enabled" => true,
+        "subscription_entitlements" => [
+          %{"feature_id" => "foo_feature"}
+        ]
+      }
+      iex(3)> Chargebeex.SubscriptionEntitlement.set_availability(subscription_id, params)
+      {:ok,
+       [%Chargebeex.SubscriptionEntitlement{...}], %{"next_offset" => nil}}
+  """
+  @spec set_availability(String.t(), set_availability_params(), keyword()) :: any()
+  def set_availability(subscription_id, params, opts \\ [])
+
+  def set_availability(
+        subscription_id,
+        %{"is_enabled" => _is_enabled, "subscription_entitlements" => _entitlements} = params,
+        opts
+      ) do
+    nested_generic_action_without_id(
+      :post,
+      [subscription: subscription_id],
+      @resource,
+      "subscription_entitlements/set_availability",
+      params,
+      opts
+    )
+  end
+
+  def set_availability(_subscription_id, _params, _opts) do
+    raise ArgumentError, """
+    Invalid params provided to `set_availability`. Ensure that `params["is_enabled"]` and `params["subscription_entitlements"]` are provided.
+    """
   end
 end
